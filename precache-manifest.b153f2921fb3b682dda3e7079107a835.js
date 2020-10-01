8d9c8f6d18120d1a1d0c05d9607677d5f4349eaa
Ganeshrockz <ganes@microsoft.com>
2020-10-02 00:09:09 +0530

    Updates

---
+++ precache-manifest.b153f2921fb3b682dda3e7079107a835.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "08577bd859c988242579",
+    "url": "/ipl-fantasy-points/static/css/main.b9b24dce.chunk.css"
+  },
+  {
+    "revision": "08577bd859c988242579",
+    "url": "/ipl-fantasy-points/static/js/main.08577bd8.chunk.js"
+  },
+  {
+    "revision": "7a120c796ff37bd32dd2",
+    "url": "/ipl-fantasy-points/static/js/runtime~main.7a120c79.js"
+  },
+  {
+    "revision": "884f9fd565905e8ffb2f",
+    "url": "/ipl-fantasy-points/static/css/2.5f425caa.chunk.css"
+  },
+  {
+    "revision": "884f9fd565905e8ffb2f",
+    "url": "/ipl-fantasy-points/static/js/2.884f9fd5.chunk.js"
+  },
+  {
+    "revision": "3faa4d45f2c7f717dc096b270e2629fb",
+    "url": "/ipl-fantasy-points/index.html"
+  }
+];
